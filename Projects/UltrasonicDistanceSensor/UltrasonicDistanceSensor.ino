1abd65260c4696d1a9a94d7c0a7c2c11b545dcdc
Dave <dskura@gmail.com>
2023-03-01 17:26:34 -0500

    Create UltrasonicDistanceSensor.ino

---
+++ Projects/UltrasonicDistanceSensor/UltrasonicDistanceSensor.ino
@@ -0,0 +1,34 @@
+/*arduino:avr:uno
+  Dave Skura, 2022
+	Ultrasonic Distance Sensor - HC-SR04
+
+*/
+const int trigPin = 9;
+const int echoPin = 10;
+
+// defines variables
+long gbl_duration;
+int gbl_distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  gbl_duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  gbl_distance = gbl_duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(gbl_distance);
+	delay(2000);
+}
